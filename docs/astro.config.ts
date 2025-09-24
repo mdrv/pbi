ffb7e6b989f6f74ff425719967fa937bc529ebae
Umar Alfarouk <medrivia@gmail.com>
2025-09-24 18:02:42 +0700

    Forgot to add docs

---
+++ docs/astro.config.ts
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import { vitePreprocess } from '@astrojs/svelte'
+import starlight from '@astrojs/starlight'
+import svelte from '@astrojs/svelte'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://pbi.bearcu.id',
+	base: '/', // /docs
+	trailingSlash: 'ignore',
+	integrations: [
+		svelte({
+			preprocess: vitePreprocess(),
+		}),
+		starlight({
+			title: 'PBI Informatics',
+			defaultLocale: 'id',
+			sidebar: [
+			],
+			customCss: ['./src/styles/global.css'],
+		}),
+	],
+	vite: {
+		plugins: [tailwindcss()],
+		server: {
+			allowedHosts: ['.vn', 'ppc.bearcu.id', '.alfarouk.id'],
+		},
+	},
+})
